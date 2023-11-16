cd2da2404b987778ba543239cbe7f1e55ba7e41f
rakesh <rakesh@DESKTOP-R0S1PET>
2023-11-16 11:13:45 +0530

    engineer2 first commit

---
+++ src/test/java/testcases/Tc2Test.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test 
+{
+  @Test
+  public void tc2()
+  {
+	  Reporter.log("tc2 created by drama",true);
+  }
+}
