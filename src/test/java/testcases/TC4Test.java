3c5f2e465e59086874fd79c836177394c5239563
rakesh <rakesh@DESKTOP-R0S1PET>
2023-11-16 12:24:58 +0530

    Enginner 01 third commit

---
+++ src/test/java/testcases/TC4Test.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC4Test 
+{
+  @Test
+  public void m1()
+  {
+	  Reporter.log("hello mama",true);
+  }
+}
