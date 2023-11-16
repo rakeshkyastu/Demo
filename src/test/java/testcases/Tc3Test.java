8d9c4b1f36ba297491395de8a73db4242ffbe944
rakesh <rakesh@DESKTOP-R0S1PET>
2023-11-16 11:27:47 +0530

    Engineer 1 2nd commit

---
+++ src/test/java/testcases/Tc3Test.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3Test 
+{
+  @Test
+  public void tc3()
+  {
+	  Reporter.log("Engineer1 2nd commit",true);
+  }
+}
