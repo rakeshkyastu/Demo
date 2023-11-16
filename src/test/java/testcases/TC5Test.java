b08131ccc558331b4ad69e0aa8035bec2c6e9198
rakesh <rakesh@DESKTOP-R0S1PET>
2023-11-16 12:32:29 +0530

    engineer 01 thid commit

---
+++ src/test/java/testcases/TC5Test.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC5Test 
+{
+  @Test
+  public void eme()
+  {
+	  Reporter.log("hello madam ji how are you",true);
+  }
+}
